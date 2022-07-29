51c7cf0bea624a2dd4bc7f0fe9b81ae159ec0f08
wr4ck3r <wr4ck3r@proton.me>
2022-07-29 22:50:18 +0300

    fix crash when there is no monitors

--- pyfetch.py
+++ pyfetch.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-import psutil
-import os
-
 from modules import distro_decorations as dd
 
 from datetime import timedelta
@@ -11,6 +8,9 @@ from datetime import datetime
 from screeninfo import get_monitors
 
 from distro import LinuxDistribution
+
+from psutil import cpu_percent, cpu_count, cpu_freq, virtual_memory, boot_time
+from os import uname
 
 def format_info(parameter, value, colors):
     pcolor, vcolor, bold, fgreset, boldreset = (
@@ -39,26 +39,30 @@ def fetch_info():
     dcolor = dd.colors
     dcolors = decor["colors"]
 
-    uptime = timedelta(seconds=datetime.now().timestamp() - psutil.boot_time())
+    uptime = timedelta(seconds=datetime.now().timestamp() - boot_time())
 
-    monitors = get_monitors()
-    if len(monitors) > 1:
-        resolutions = ""
-        for m in monitors():
-            resolutions += f"{m.width}x{m.height} @ "
-        resolutions = resolutions[:-3]
-    else:
-        resolutions = f"{monitors[0].width}x{monitors[0].height}   "
+    try:
+        monitors = get_monitors()
+        if len(monitors) > 1:
+            resolutions = ""
+            for m in monitors():
+                resolutions += f"{m.width}x{m.height} @ "
+            resolutions = resolutions[:-3]
+        else:
+            resolutions = f"{monitors[0].width}x{monitors[0].height}   "
+    except:
+        resolutions = "no monitors"
 
-    uname = os.uname()
+    ouname = uname()
+    ram = virtual_memory()
 
     parameters = {
-        "CPU": f"{psutil.cpu_percent(0.1)}% [{psutil.cpu_count()}] {round(psutil.cpu_freq().max / 1000, 1)} GHz",
-        "RAM": f"{round(psutil.virtual_memory().used / 8**10, 1)}G / {round(psutil.virtual_memory().total / 8**10, 1)}G ({round(psutil.virtual_memory().total / 8**10)}G)",
+        "CPU": f"{cpu_percent(0.1)}% [{cpu_count()}] {round(cpu_freq().max / 1000, 1)} GHz",
+        "RAM": f"{round(ram.used / 8**10, 1)}G / {round(ram.total / 8**10, 1)}G ({round(ram.total / 8**10)}G)",
         "Resolution": resolutions[:-3],
         "Uptime": f"{round(uptime.seconds / 60**2, 1)} hr",
         "OS": os_name,
-        "Kernel": f'{uname[2]} [{uname[4]}]'
+        "Kernel": f'{ouname[2]} [{ouname[4]}]'
     }
 
     pcolor, vcolor, logo_color = (
